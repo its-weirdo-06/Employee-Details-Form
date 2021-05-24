0638cffc6a2ccdcf7688ccaa64e7b99c07bde884
Prasad Badhe <83410561+its-weirdo-06@users.noreply.github.com>
2021-05-24 14:54:06 +0530

    Add files via upload

---
+++ Slip 15.php
@@ -0,0 +1,12 @@
+<?php
+ $name=$_GET['ename'];
+ $des=$_GET['ed'];
+ $dep=$_GET['dep'];
+ $g=$_GET['Gen'];
+ $Salary=$_GET['esal'];
+ echo "Name : ".$name."<br>";
+ echo "Designation : ".$des."<br>";
+ echo "Department :  ".$dep."<br>";
+ echo "Gender : ".$g."<br>";
+ echo "Salary : ".$Salary."<br>";
+?>
